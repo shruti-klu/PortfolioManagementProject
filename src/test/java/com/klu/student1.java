e1dfb366bf4250b5be52086175718c185afb9adb
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-09 19:33:30 +0530

    Added student1 constructors example

---
+++ src/test/java/com/klu/student1.java
@@ -0,0 +1,19 @@
+package com.klu;
+
+public class student1 {
+	public student1()  { //default constructor
+	System.out.println("Hi welcome to my class");
+}
+public student1(int x)//para cont
+{
+System.out.println(x);
+}
+
+	public static void main(String[] args) {
+		student1 s=new student1();
+		student1 s1=new student1(10);
+		// TODO Auto-generated method stub
+
+	}
+
+}
