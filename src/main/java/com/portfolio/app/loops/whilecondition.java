4877bcbbf408a8083b9e910cd8604e4a123f00e2
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-13 11:38:45 +0530

    added while condition

---
+++ src/main/java/com/portfolio/app/loops/whilecondition.java
@@ -0,0 +1,14 @@
+package com.portfolio.app.loops;
+
+class whilecondition {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<11) {
+			System.out.println(i);
+			i=i+1;  i ++; 
+		}
+
+	}
+
+}
