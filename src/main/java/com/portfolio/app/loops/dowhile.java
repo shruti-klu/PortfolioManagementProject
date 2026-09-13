8fa22761df21b1972129a28412945e4b21703803
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-13 11:47:32 +0530

    added do while

---
+++ src/main/java/com/portfolio/app/loops/dowhile.java
@@ -0,0 +1,15 @@
+package com.portfolio.app.loops;
+
+public class dowhile {
+
+	public static void main(String[] args) {
+		int i=0;
+		do {
+			System.out.println(i);
+			i=i+1;
+		}while(i<11);
+		}
+
+	}
+
+
