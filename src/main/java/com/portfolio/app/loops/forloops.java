2cd6fc79ce2c4167a4730b497a58473673e6b57f
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-13 10:54:49 +0530

    added for loop code

---
+++ src/main/java/com/portfolio/app/loops/forloops.java
@@ -0,0 +1,12 @@
+package com.portfolio.app.loops;
+
+public class forloops {
+
+	public static void main(String[] args) {
+		for(int Counter=0; Counter<100;Counter=Counter+1) {
+			System.out.println("Hello world");
+		}
+
+	}
+
+}
