66a23feae07529f0585e5bf550aaeb9a4fbe65fc
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-15 18:47:51 +0530

    added asset

---
+++ src/main/java/com/portfolio/app/model/Asset.java
@@ -0,0 +1,33 @@
+package com.portfolio.app.model;
+
+public abstract class Asset {
+	private String assetid;
+	private String assetName;
+	private double purchasePrice;
+	
+	//constructor
+	public Asset(String assetId,String assetName,double purchasePrice) {
+		
+		this.assetid = assetid;
+		this.assetName=assetName;
+		this.purchasePrice=purchasePrice;
+	}
+	//Getters
+	public String getAssetid() {
+		return assetid;
+	}
+	public String getAsssetName() {
+		return assetName;
+	}
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+	//Abstract method
+	public abstract double calculateCurrentValue();
+
+	public static void main(String[] args) {
+		System.out.println("Asset class is workiing");
+
+	}
+
+}
