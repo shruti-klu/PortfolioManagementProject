5f1a68b6f90fc39dde118b3c30451b9cc5447ec0
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-20 19:08:14 +0530

    added holding or main

---
+++ src/main/java/com/portfolio/app/Main.java
@@ -0,0 +1,13 @@
+package com.portfolio.app;
+import com.portfolioproject.model.User;
+import com.portfolioproject.model.stock;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/portfolioproject/model/Holding.java
@@ -0,0 +1,11 @@
+package com.portfolioproject.model;
+
+import com.portfolioproject.app.Stock;
+
+public class Holding {
+
+	public Holding(Holding stockHolding, Stock stock, int quantity) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
