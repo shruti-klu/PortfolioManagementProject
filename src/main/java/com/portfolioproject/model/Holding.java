a1d18c2d5602ad260ef7d5fc98e16185053d7562
Shruti kumari <shruti2022jyg@gmail.com>
2026-09-20 19:15:52 +0530

    added holding

--- src/main/java/com/portfolioproject/model/Holding.java
+++ src/main/java/com/portfolioproject/model/Holding.java
@@ -1,11 +1,17 @@
 package com.portfolioproject.model;
 
-import com.portfolioproject.app.Stock;
-
 public class Holding {
+    public String symbol;
+    public int quantity;
+    public double buyPrice;
 
-	public Holding(Holding stockHolding, Stock stock, int quantity) {
-		// TODO Auto-generated constructor stub
-	}
-
+    public Holding(String symbol, int quantity, double buyPrice) {
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyPrice = buyPrice;
+    }
+    
+    public String toString() {
+        return symbol + " - " + quantity + " qty @ " + buyPrice;
+    }
 }
